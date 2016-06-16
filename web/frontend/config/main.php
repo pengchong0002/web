44941f9e0955c2e87863da6f8a983f8b8167d1c5
pengchong0002 <2494032383@qq.com>
2016-06-16 09:21:58 +0800

    修改

--- web/frontend/config/main.php
+++ web/frontend/config/main.php
@@ -29,7 +29,7 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'enableStrictParsing' => false,
