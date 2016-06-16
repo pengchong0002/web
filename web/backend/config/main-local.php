457cb6bc88968581f1707e29f487227288766ef5
zhangpei123 <1151556628@qq.com>
2016-06-16 08:37:22 +0800

    张佩

--- web/backend/config/main-local.php
+++ web/backend/config/main-local.php
@@ -1,6 +1,7 @@
 <?php
 
 $config = [
+'defaultRoute'=>'public/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
