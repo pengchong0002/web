4ba324f179aab40f6180779c0eea68ee3df0e957
zhangpei123 <1151556628@qq.com>
2016-06-23 08:21:11 +0800

    张佩

--- web/backend/controllers/PublicController.php
+++ web/backend/controllers/PublicController.php
@@ -2,11 +2,29 @@
 
 namespace backend\controllers;
 
+use Yii;
 class PublicController extends \yii\web\Controller
 {
-    public function actionIndex()
-    {
-        return $this->renderPartial('index');
-    }
+		/* author:张佩
+		* controller:public
+		* meaning:后台登录界面
+		*creatime:2016年六月二十七号
+		*/
+	    public function actionIndex()
+	    {
+	        return $this->renderPartial('login');
+	    }
+		/* author:张佩
+		* controller:public
+		* meaning:管理员退出
+		* creatime:2016年六月十七号
+		*/
+		public function actionQuit()
+		{
+			unset(yii::$app->session['u_name']);
+			return $this->renderPartial('login');
+		}
+
+   
 
 }
